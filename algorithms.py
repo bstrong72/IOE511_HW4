43f5f81208676a2b26e8d35dead40718ba9f175c
bstrong72 <79659587+bstrong72@users.noreply.github.com>
2023-03-26 23:00:32 -0400

    GD Update
    Updated GD Step algorithm to work better for HW4.

--- algorithms.py
+++ algorithms.py
@@ -1,23 +1,25 @@
 # Compute the next step for all iterative optimization algorithms given current solution x:
 # (1) Gradient Descent
+# (2) Stochastic Gradient
 
 import functions as fnc
 import numpy as np
 
 
-def GDStep(x, f, g, problem, method, options):
+def GDStep(w, f, g, X, y, problem, method, options):
     # Set the search direction d to be -g
     d = -g
-
+    X = problem.X
+    y = problem.y
     # determine step size
     if method.step_type == 'Constant':
         # set alphas and compute new x based on step size
         alpha = method.constant_step_size
-        x_new = x + alpha * d
+        w_new = w + alpha * d
 
         # Compute new values of f, g , H
-        f_new = problem.compute_f(x_new)
-        g_new = problem.compute_g(x_new)
+        f_new = problem.compute_f(w_new, X, y)
+        g_new = problem.compute_g(w_new, X, y)
 
     elif method.step_type == 'Backtracking':
         # initialize constants for backtracking subroutine
@@ -26,17 +28,17 @@ def GDStep(x, f, g, problem, method, options):
         Tau = 0.5
 
         # search for new alpha until subroutine conditions are met
-        while problem.compute_f(x + (alpha * d)) > (f + (c1 * alpha * np.matmul(g, np.transpose(d)))):
+        while problem.compute_f(w + (alpha * d), X, y) > (f + (c1 * alpha * np.matmul(g, np.transpose(d)))):
             alpha = Tau * alpha
         # compute new x based on step size
-        x_new = x + alpha * d
+        w_new = w + alpha * d
         # Compute new values of f, g , H
-        f_new = problem.compute_f(x_new)
-        g_new = problem.compute_g(x_new)
+        f_new = problem.compute_f(w_new, X, y)
+        g_new = problem.compute_g(w_new, X, y)
     else:
         print('Warning: step type is not defined')
 
-    return x_new, f_new, g_new, d, alpha
+    return w_new, f_new, g_new, d, alpha
 
 
 def SGDStep(w, loss_f, loss_g, X, y, alpha, problem, method, options):
@@ -58,3 +60,4 @@ def SGDStep(w, loss_f, loss_g, X, y, alpha, problem, method, options):
     loss_f_new = None
     loss_g_new = None
     return w_new, loss_f_new, loss_g_new, d, alpha
+
